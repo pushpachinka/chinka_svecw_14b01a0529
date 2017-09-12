13f1ccb478c012e781a5419ee2875713ffc616f1
pushpachinka <pushpachinka@gmail.com>
2017-09-12 03:08:08 -0700

    commit 11

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -165,23 +165,6 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public void removeFromElement(Student student) {
 		// Add your implementation here
-		try {
-			if(student == null)
-				throw new IllegalArgumentException();
-			int i;
-			for(i = 0; i < students.length; i++) {
-				if(students[i] == student) {
-					break;
-				}
-			}
-			Student[] temp = new Student[i];
-			for(int j = 0; j < i; j++) {
-				temp[j] = students[j];
-			}
-			students = temp;
-		}catch(IllegalArgumentException iae) {
-			iae.printStackTrace();
-		}
 	}
 
 	@Override
